78064a1314317ac6a0f4499b78f39f331402f8d6
Danielfile <m15603371251@163.com>
2019-03-25 18:13:41 +0800

    finished shoolgate

--- src/components/Charts/shoolgate.vue
+++ src/components/Charts/shoolgate.vue
@@ -5,172 +5,144 @@
 <script>
 import echarts from 'echarts'
 import resize from './mixins/resize'
+import { timestampToTime } from '@/utils/myDate'
 
-function dataFormatter(obj) {
-  if(init) return obj;
-  var pList = [];
-  for(var i=0; i<60; i++){
-    if(i<10) pList[i] = '0'+i;
-    else pList[i] = i+'';
-  }
-  for(var i=0; i<24; i++){
-    var now = 0;
-    var sum = 0;
-    for(var j=0; j<60; j++){
-      now = Math.round(Math.random()*19);
-      sum += now;
-      obj[i][j] = {
-          name : pList[i],
-          value : now
-      };
-    }
-  }
-  return obj;
-}
-var init = false;
-var dataGate = dataFormatter({
-  0:[], 1:[], 2:[], 3:[], 4:[], 5:[], 6:[], 7:[],
-  8:[], 9:[],10:[], 11:[], 12:[], 13:[], 14:[], 15:[],
-  16:[], 17:[], 18:[], 19:[], 20:[], 21:[], 22:[], 23:[]
-});
-
+var data = [
+  [0, 0, '西 门', '1546323001000'],
+  [0, 0, '东北门', '1546394903000'],
+  [0, 0, '东 门', '1547181799000'],
+  [0, 0, '东 门', '1550993023000'],
+  [0, 0, '西 门', '1551515967000'],
+  [0, 0, '东 门', '1552286577000'],
+  [0, 0, '西 门', '1554001297000']
+];  //data[0]、data[1]默认？不知如何修改。
 
 var option = {
-  baseOption: {
-    timeline: {
-      axisType: 'category',
-      autoPlay: true,
-      playInterval: 2000,
-      data: [
-        '8:23:03', '10:25:12', '11:45:59', '15:07:15', '15:57:15',
-        {
-          value: '18:01:21',         
-          symbol: 'diamond',
-          symbolSize: 16
-        }
-      ],
-      tooltip: {
-        formatter: '{b}'
-      },
-      label: {
-        formatter : function(s) {
-          return s.split(":")[0];
-        }
+  backgroundColor: '#cafbfe',
+  xAxis: {
+    show: false,
+    type: 'value',
+    min: 0,
+    max: 8
+  },
+  yAxis: {
+    show: false,
+    type: 'value',
+    min: 0,
+    max: 4
+  },
+  grid: {
+    bottom: 0,
+    top: 0,
+    left: 0,
+    right: 0
+  },
+  tooltip: {
+    formatter: function (obj) {
+      if(obj.name == ""){
+        return timestampToTime(Number(obj.data[3]));
       }
-    },
-    title: {
-      subtext: '2019-04-01'
+      return ;
     },
     tooltip: {
-      formatter: function (obj) {
-        if(obj.dataType=="undefined"){  //鼠标移动到条形图不显示内容
-           return ;
-        }
-      },
-      tooltip: {
-        show: true
-      }
-    },
-    toolbox: {  
-      show: true,  
-      itemSize: 20,
-      itemGap: 30,
-      right: 20,
-      orient: 'vertical',
-      color: ['#1e90ff','#22bb22','#4b0082','#d2691e'],
-      feature: {
-        saveAsImage: {  
-          pixelRatio: 2  
-        }
-      }
-    },
-    calculable : true,
-    grid: {
-      top: 80,
-      bottom: 100
-    },
-    xAxis: [
-    {
-      'type':'category',
-      'axisLabel':{'interval':0},
-      'data':[
-        '00','\n01','02','\n03','04','\n05','06','\n07','08','\n09',
-        '10','\n11','12','\n13','\n14','\n15','16','\n17','18','\n19',
-        '20','\n21','22','\n23','\n24','\n25','26','\n27','28','\n29',
-        '30','\n31','32','\n33','34','\n35','36','\n37','38','\n39',
-        '40','\n41','42','\n43','44','\n45','46','\n47','48','\n49',
-        '50','\n51','52','\n53','54','\n55','56','\n57','58','\n59'
-      ],
-      splitLine: {show: false}
+      show: true
     }
-    ],
-    yAxis: [
-      {
-        type: 'value',
-        name: '次数',
-        max: 20
+  },
+  toolbox: {  
+    show: true,  
+    itemSize: 20,
+    itemGap: 30,
+    right: 20,
+    orient: 'vertical',
+    color: ['#1e90ff','#22bb22','#4b0082','#d2691e'],
+    feature: {
+      saveAsImage: {  
+        pixelRatio: 2  
       }
-    ],
-    series: [
-      {
-        name: '', 
-        type: 'bar',
-        itemStyle: {
-          normal: {
-            color: 'rgba(66,99,129,0.8)',
-            label: {
-              show: true,
-              textStyle: {
-                color: '#009'
-              },
-              position:  [0, -10],
-              formatter(p) {
-                return p.value > 0 ? p.value : ''
+    }
+  },
+  series: [{
+    type: 'custom',
+    name: 'custom',
+    itemStyle: {
+      normal: {
+        color: new echarts.graphic.LinearGradient(0,0,0,1, [
+          {offset: 0, color: '#52a5b4'},
+          {offset: 1, color: '#14b7f0'}
+        ]),
+        shadowBlur: 20,
+        shadowOffsetX: -15,
+        shadowOffsetY: 15,
+        shadowColor: 'rgba(0,0,0,0.8)'
+      },
+      emphasis: {
+        color: new echarts.graphic.LinearGradient(0.5,0,0.5,1, [
+            {offset: 0, color: '#63E4FBCC'},
+            {offset: 1, color: '#028BBBCC'}
+        ])
+      }
+    },
+    renderItem: function(params, api) {
+      var value = api.value(2);
+      var name = timestampToTime(api.value(3)).split(' ')[0];
+      var d = 30 * Math.random() + 40;
+      var coord = api.coord([params.dataIndexInside + 1, (params.dataIndexInside) % 3 + 1]);
+      var x = coord[0];
+      var y = coord[1];
+      var points = {
+        left: [x - d, y],
+        right: [x + d, y],
+        top: [x, y - d],
+        bottom: [x, y + d]
+      }
+      var labelWidth = 17;
+      var labelHeight = 9;
+      return {
+        type: 'group',
+        children: [
+          {
+            type: 'polygon',
+            shape: {
+              points: [points.left, points.top, points.right, points.bottom, points.left]
+            },
+            style: api.style(),
+            styleEmphasis: api.styleEmphasis()
+          },
+          {
+            type: 'group',
+            children: [
+              {
+                type: 'text',
+                style: {
+                  text: value,
+                  x: x - labelWidth,
+                  y: y - labelHeight,
+                  fill: '#fff',
+                  font: 'normal 16px "Microsoft YaHei", sans-serif'
+                }
               }
+            ]
+          },
+          {
+            type: 'text',
+            style: {
+              text: name,
+              x: x - (name.length * 5),
+              y: y - d - 18,
+              fill: '#8492A6',
+              font: 'normal 16px "Microsoft YaHei", sans-serif'
             }
           }
-        }
+        ]
       }
-    ]
-  },
-  options: [
-    {
-      title: {text: '星期天'},
-      series: [
-        {data: dataGate[8]}
-      ]
     },
-    {
-      title : {text: '星期天'},
-      series : [
-        {data: dataGate[10]}
-      ]
-    },
-    {
-      title: {text: '星期天'},
-      series: [
-        {data: dataGate[11]}
-      ]
-    },
-    {
-      title : {text: '星期天'},
-      series : [
-        {data: dataGate[15]}
-      ]
-    },
-    {
-      title: {text: '星期天'},
-      series: [
-        {data: dataGate[15]}
-      ]
-    },
-    {
-      title : {text: '星期天'},
-      series : [
-        {data: dataGate[18]}
-      ]
+    data: data,
+    animationDuration: 1500,
+    animationEasing: 'sinusoidalInOut',
+    animationDelay: function(idx) {
+      return idx * 300;
     }
-  ]
+  }]
 };
 
 
